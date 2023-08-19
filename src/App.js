15919fdc594c00a8948bfcb4dc8d4a96e185c736
kalkM <yidreslekali@gmail.com>
2023-08-19 11:52:55 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <br />
-      <footer>
-        <a
-          href="https://github.com/KalkM/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open source code
-        </a>{" "}
-        by Kalkidan Mulugeta Diro
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <br />
+        <footer>
+          <a
+            href="https://github.com/KalkM/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open source code
+          </a>{" "}
+          by Kalkidan Mulugeta Diro
+        </footer>
+      </div>
     </div>
   );
 }
